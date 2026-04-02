321b249b9f2e27b25eda4c8ebe790cd09014f5c4
zoujia123 <103401577+zoujia123@users.noreply.github.com>
2026-04-02 20:18:14 +0800

    test original custom

--- custom.py
+++ custom.py
@@ -13,10 +13,10 @@ import torch.utils.data as data
 
 from config.parser import parse_args
 
-import datasets
-from raft import RAFT
-from utils.flow_viz import flow_to_image
-from utils.utils import load_ckpt
+# import datasets
+from core.raft import RAFT
+from core.utils.flow_viz import flow_to_image
+from core.utils.utils import load_ckpt
 
 def create_color_bar(height, width, color_map):
     """
